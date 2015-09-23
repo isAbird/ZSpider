dff30bc8b77a35eb8a9d29b274aee7b714397531
杜身星 <isabird@163.com>
2015-09-23 09:53:18 +0800

    创建工程

---
+++ ZSpider/src/main/java/BUU/ZSpider/App.java
@@ -0,0 +1,13 @@
+package BUU.ZSpider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
